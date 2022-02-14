f5d42803776e12cb7dceb909cd7e23d0627e5e38
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-14 15:41:06 -0700

    fixed db

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,14 +3,22 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using WaterProject.Models;
 
 namespace WaterProject.Controllers
 {
     public class HomeController : Controller
     {
+
+        private WaterProjectContext context {get; set;}
+
+        public HomeController(WaterProjectContext temp) => context = temp;
         public IActionResult Index()
         {
-            return View();
+            var data = context.Projects.ToList();
+            return View(data);
         }
+
+
     }
 }
--- Models/Project.cs
+++ Models/Project.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -7,11 +8,13 @@ namespace WaterProject.Models
 {
     public partial class Project
     {
-        public long ProjectId { get; set; }
+        [Key]
+        [Required]
+        public int ProjectId { get; set; }
         public string ProjectName { get; set; }
         public string ProjectType { get; set; }
-        public long? ProjectRegionalProgram { get; set; }
-        public long? ProjectImpact { get; set; }
+        public string ProjectRegionalProgram { get; set; }
+        public int ProjectImpact { get; set; }
         public string ProjectPhase { get; set; }
         public string ProjectFunctionalityStatus { get; set; }
     }
