8d392871feeaa80d665fc5d491f24594d0a9a8fb
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-14 14:40:44 -0700

    set up db

---
+++ Models/Project.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace WaterProject.Models
+{
+    public partial class Project
+    {
+        public long ProjectId { get; set; }
+        public string ProjectName { get; set; }
+        public string ProjectType { get; set; }
+        public long? ProjectRegionalProgram { get; set; }
+        public long? ProjectImpact { get; set; }
+        public string ProjectPhase { get; set; }
+        public string ProjectFunctionalityStatus { get; set; }
+    }
+}
