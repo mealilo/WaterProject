087f73289ebaccdc2c9e59b24288a68608987aa9
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-23 15:07:02 -0700

    basket, redirect ,json

--- Pages/Donate.cshtml.cs
+++ Pages/Donate.cshtml.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WaterProject.Infrastructure;
 using WaterProject.Models;
 
 namespace WaterProject.Pages
@@ -18,22 +19,26 @@ namespace WaterProject.Pages
             repo = temp;
         }
 
-        public Basket basket { get; set; }
-        public void OnGet(Basket b)
+        public Basket basket { get; set;}
+        public string ReturnURl { get; set; }
+        public void OnGet(string returnURl)
         {
-            basket = b;
+            ReturnURl = returnURl ?? "/";
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
         }
 
-        public IActionResult OnPost(int projectID)
+        public IActionResult OnPost(int projectID, string returnUrl)
         {
 
             Project p = repo.Projects.FirstOrDefault(x => x.ProjectId == projectID);
 
-            basket  = new Basket();
+            basket = HttpContext.Session.GetJson<Basket>("basket") ?? new Basket();
 
             basket.AddItem(p, 1);
 
-            return RedirectToPage(basket);
+            HttpContext.Session.SetJson("basket", basket);
+
+            return RedirectToPage(new { ReturnURl = returnUrl });
 
         }
     }
