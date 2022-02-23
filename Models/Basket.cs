5da197ca7a53f3e99660808622df1edb9e7f9268
mealilo <90289295+mealilo@users.noreply.github.com>
2022-02-23 14:20:26 -0700

    basket

---
+++ Models/Basket.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterProject.Models
+{
+    public class Basket
+    {
+
+        public List<BasketLineItem> Items { get; set; } = new List<BasketLineItem>();
+
+        public void AddItem(Project proj, int qty)
+        {
+            BasketLineItem Line = Items
+                .Where(p => p.Project.ProjectId == proj.ProjectId)
+                .FirstOrDefault();
+
+            if (Line == null)
+            {
+                Items.Add(new BasketLineItem
+                {
+                    Project = proj,
+                    Quantity = qty
+                });
+            }
+            else
+            {
+                Line.Quantity += qty;
+            }
+        }
+
+        public double CalculateTotal()
+        {
+            double sum = Items.Sum(x => x.Quantity * 25);
+            return sum;
+        }
+        
+    }
+
+    public class BasketLineItem
+    {
+        public int LineID { get; set; }
+        public Project Project { get; set; }
+        public int Quantity { get; set; }
+    }
+}
--- Pages/Donate.cshtml.cs
+++ Pages/Donate.cshtml.cs
@@ -4,13 +4,37 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using WaterProject.Models;
 
 namespace WaterProject.Pages
 {
     public class DonateModel : PageModel
     {
-        public void OnGet()
+
+        private IWaterProjectRepository repo { get; set; }
+
+        public DonateModel (IWaterProjectRepository temp)
         {
+            repo = temp;
+        }
+
+        public Basket basket { get; set; }
+        public void OnGet(Basket b)
+        {
+            basket = b;
+        }
+
+        public IActionResult OnPost(int projectID)
+        {
+
+            Project p = repo.Projects.FirstOrDefault(x => x.ProjectId == projectID);
+
+            basket  = new Basket();
+
+            basket.AddItem(p, 1);
+
+            return RedirectToPage(basket);
+
         }
     }
 }
